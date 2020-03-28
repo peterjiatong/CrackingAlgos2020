d9782cedca1d7798e3a7f1000ce82dfd7144fe8c
Tong Jia <tongjia@TongdeMacBook-Pro.local>
2020-03-27 19:36:02 -0700

    3/27 02

---
+++ src/main/java/Lesson11182019/L240SearchA2DMatrixII.java
@@ -0,0 +1,22 @@
+package Lesson11182019;
+
+public class L240SearchA2DMatrixII {
+    public boolean searchMatrix(int[][] matrix, int target) {
+
+        if (matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        } else {
+            for (int i = matrix.length - 1; i >= 0; i--) {
+                if (matrix[i][0] <= target) {
+                    for (int i1 = 0; i1 < matrix[i].length; i1++) {
+                        if (matrix[i][i1] == target) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+
+    }
+}

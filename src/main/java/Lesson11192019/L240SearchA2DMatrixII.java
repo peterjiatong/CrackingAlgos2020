89573d07c5a6a0158b05ddf7264fc111dc62d0ca
Tong Jia <tongjia@TongdeMacBook-Pro.local>
2020-03-29 21:51:07 -0700

    3/29 02

--- src/main/java/Lesson11192019/L240SearchA2DMatrixII.java
+++ src/main/java/Lesson11192019/L240SearchA2DMatrixII.java
@@ -1,7 +1,12 @@
 package Lesson11192019;
 
 public class L240SearchA2DMatrixII {
-    public boolean searchMatrix(int[][] matrix, int target) {
+
+    public static void main(String[] args) {
+        int [] [] matrix = new int[][] {{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}};
+        searchMatrix(matrix,5);
+    }
+    public static boolean searchMatrix(int[][] matrix, int target) {
 
         if (matrix.length == 0 || matrix[0].length == 0) {
             return false;
@@ -9,7 +14,11 @@ public class L240SearchA2DMatrixII {
             for (int i = matrix.length - 1; i >= 0; i--) {
                 if (matrix[i][0] <= target) {
                     for (int i1 = 0; i1 < matrix[i].length; i1++) {
-                        if (matrix[i][i1] == target) {
+                        if(matrix[i][i1] > target) {
+                            i--;
+                            i1--;
+                        }
+                        else if (matrix[i][i1] == target) {
                             return true;
                         }
                     }
--- src/main/java/Lesson11192019/L74SearchA2DMatrix.java
+++ src/main/java/Lesson11192019/L74SearchA2DMatrix.java
@@ -1,21 +1,47 @@
 package Lesson11192019;
 
+
+import javax.management.remote.JMXAddressable;
+
 public class L74SearchA2DMatrix {
     public boolean searchMatrix(int[][] matrix, int target) {
-
         if (matrix.length == 0 || matrix[0].length == 0) {
             return false;
-        } else {
-            for (int i = matrix.length - 1; i >= 0; i--) {
-                if (matrix[i][0] <= target) {
-                    for (int i1 = 0; i1 < matrix[i].length; i1++) {
-                        if (matrix[i][i1] == target) {
-                            return true;
-                        }
-                    }
-                }
+        }
+        int left = 0;
+        int right = matrix.length * matrix[0].length - 1;
+
+
+        while (left <= right) {
+            int mid = (right + left) / 2;
+            int X = mid/ matrix[0].length;
+            int Y = mid % matrix[0].length;
+
+            if (matrix[X][Y] == target) {
+                return true;
+            } else if (matrix[X][Y] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
         return false;
     }
 }
+/*
+if(matrix.length == 0 || matrix[0].length == 0){
+        return false;
+        }
+        else {
+        for (int i = matrix.length-1; i >= 0 ; i--) {
+        if (matrix[i][0] <= target)
+        {
+        for (int i1 = 0; i1 < matrix[i].length; i1++) {
+        if (matrix[i][i1] == target){
+        return true;
+        }
+        }
+        }
+        }
+        }
+        return false;*/
